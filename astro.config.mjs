39a06ec19bfd7e054308843f461ecd9963f0ce25
Jabreu96 <ajoelcrist@gmail.com>
2023-10-05 18:46:16 -0400

    Added endpoint for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   // ...
+  site: "jabreu96.github.io",
+  base: "/astro-portfolio"
   assetsInclude: ['**/*.pdf'],
   publicDir: './public',
   postcss: [tailwind('./tailwind.config.js')
